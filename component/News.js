bf9eccd6d805b9a0227e6efc1cfa214ab1101221
parth <parth305pandya@gmail.com>
2022-04-09 14:12:28 +0530

    Infinite Scroll

--- component/News.js
+++ component/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
     static defaultProps = {
@@ -18,85 +20,59 @@ export default class News extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            api: {
-                article: [],
-                totalResults:0,
-                page: 1
-            },
-            loading: false
+            article: [],
+            totalResults: 0,
+            page: 1,
+            loading: true
         }
-        
+
         document.title = `NewsMonkey- ${this.props.category}`
     }
 
-    async componentDidMount() {
+    fetchMoreData = async () => {
         this.setState({
-            api:this.state.api,
-            loading:true
+            loading: false
         })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a0d52ad867b94a9e89044ed9d2c2541f&pagesize=${this.props.pagesize}&page=${this.state.page}`;
+        let data = await fetch(url);
+        let parseddata = await data.json();
+        console.log(parseddata);
+        this.setState({
+            article: this.state.article.concat(parseddata.articles),
+            page: this.state.page + 1,
+            totalResults: parseddata.totalResults,
+            loading: false
+        })
+        console.log(parseddata);
+    }
+    async componentDidMount() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a0d52ad867b94a9e89044ed9d2c2541f&pagesize=${this.props.pagesize}`;
         let data = await fetch(url);
         let parseddata = await data.json();
         this.setState({
-            api:{
             article: parseddata.articles,
-            page: 1
-        },
-        loading: false
-        })
-    }
-
-    handlenext = async () => {
-        this.setState({
-            api:this.state.api,
-            loading:true
-        })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a0d52ad867b94a9e89044ed9d2c2541f&page=${this.state.api.page + 1}&pagesize=${this.props.pagesize}`;
-        let data = await fetch(url);
-        let parseddata = await data.json();
-        if (parseddata.totalResults === (this.state.api.page * this.props.pagesize + parseddata.articles.length)) {
-            document.getElementById("next").disabled = true
-        }
-        this.setState({
-            api:{
-            article: parseddata.articles,
-            page: this.state.api.page + 1,
-            totalResults:parseddata.totalResults
-        },
-        loading: false
-        })
-
-    }
-    handleprev = async () => {
-        this.setState({
-            api: this.state.api,
-            loading: true
-        })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a0d52ad867b94a9e89044ed9d2c2541f&page=${this.state.api.page - 1}&pagesize=${this.props.pagesize}`;
-        let data = await fetch(url);
-        let parseddata = await data.json();
-        this.setState({
-            api: {
-                article: parseddata.articles,
-                page: this.state.api.page - 1,
-                totalResults:parseddata.totalResults
-            },
+            page: 1,
+            totalResults: parseddata.totalResults,
             loading: false
         })
-
-        if (document.getElementById("next").disabled) {
-            document.getElementById("next").disabled = false
-        }
-
     }
+
+
     render() {
         return (
-            <div className='container my-3'>
+            <>
                 <h2 style={{ color: this.props.mode === "dark" ? "white" : "black", margin: "35px 0px" }} className="text-center">NewsMonkey - quike news for you</h2>
                 {this.state.loading && <Spinner/>}
-                    {!this.state.loading &&
+                <InfiniteScroll
+                    dataLength={this.state.article.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.article.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    {
+                        <div className='container my-3'>
                         <div className="row">
-                            {this.state.api.article.map(Element => {
+                            {this.state.article.map(Element => {
                                 if (Element) {
                                     return (
                                         <div className="col-md-3 mx-5 my-3" key={Element.url}>
@@ -110,13 +86,10 @@ export default class News extends Component {
                                 }
                             })}
                         </div>
+                    </div>
                     }
-                
-                <div className="container d-flex justify-content-between my-3" >
-                    <button type="button" disabled={this.state.api.page > 1 ? false : true} id="pre" className="btn btn-primary" onClick={this.handleprev}>{"<"} Previous</button>
-                    <button type="button" id="next" className="btn btn-primary" onClick={this.handlenext} style={{ outline: "none" }}>Next {">"}</button>
-                </div>
-            </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
--- component/Spinner.js
+++ component/Spinner.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import loading from "./loading.svg"
+import loading from "./Spinner-1s-257px.svg"
 
 export default class spinner extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img src={loading} alt="loading" />
+        <img className='my-5' src={loading} alt="loading" />
       </div>
     )
   }
